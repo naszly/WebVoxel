4c3f6ff60e1e1df5533563aad5e32891bafb1617
naszly <naszaly.k@gmail.com>
2025-05-15 21:40:55 +0200

    Change Chunk generation logic to handle negative Y positions

--- client/src/world/Chunk.cpp
+++ client/src/world/Chunk.cpp
@@ -9,23 +9,31 @@
 void Chunk::generate(const FastNoise::SmartNode<> &fnGenerator) {
     const Timer timer("Chunk::generate");
 
-    if (m_Position.y >= 0) {
-        std::vector<float> noise(Chunk::SIZE * Chunk::SIZE);
+    if (m_Position.y >= 0 && m_Position.y * SIZE < 256) {
+        std::vector<float> noise(SIZE * SIZE);
 
-        const size_t xStart = m_Position.z * Chunk::SIZE;
-        const size_t yStart = m_Position.x * Chunk::SIZE;
-        fnGenerator->GenUniformGrid2D(noise.data(), xStart, yStart, Chunk::SIZE, Chunk::SIZE, 0.0004f, 0);
+        const size_t xStart = m_Position.z * SIZE;
+        const size_t yStart = m_Position.x * SIZE;
+        fnGenerator->GenUniformGrid2D(noise.data(), xStart, yStart, SIZE, SIZE, 0.0004f, 0);
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 for (int k = 0; k < SIZE; k++) {
-                    const int noiseValue = static_cast<int>((noise[i * SIZE + k] * 0.5 + 0.5) * 512);
+                    const int noiseValue = static_cast<int>((noise[i * SIZE + k] * 0.5 + 0.5) * 255);
                     const int height = m_Position.y * SIZE + j;
                     if (noiseValue == height) {
                         m_Data.setVoxel(i, j, k, VoxelData(0, 160 + (random() % 64), 0));
-                    } else if (noiseValue >  height) {
+                    } else if (noiseValue > height) {
                         m_Data.setVoxel(i, j, k, VoxelData(135 + (random() % 20 - 10), 69 + (random() % 20 - 10), 19 + (random() % 20 - 10)));
                     }
+                }
+            }
+        }
+    } else if (m_Position.y < 0) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                for (int k = 0; k < SIZE; k++) {
+                    m_Data.setVoxel(i, j, k, VoxelData(66 + (random() % 8), 67 + (random() % 8), 69 + (random() % 10)));
                 }
             }
         }
