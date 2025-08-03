03631f4b42f05ddc9038b3f2abe1f42f2b8771a6
naszly <naszaly.k@gmail.com>
2025-08-03 08:28:24 +0200

    Move IdType class to a new header file

--- client/src/common/datastructures/DataIdMapper.h
+++ client/src/common/datastructures/DataIdMapper.h
@@ -5,53 +5,8 @@
 #include <vector>
 #include <unordered_map>
 
+#include "IdType.h"
 #include "common/Utils.h"
-
-enum class IdSize : uint8_t {
-    U8Bit = 8,
-    U16Bit = 16,
-    U20Bit = 20
-};
-
-template<IdSize Size>
-class IdType {
-public:
-    using UintT = std::conditional_t<
-        Size == IdSize::U8Bit,
-        uint8_t,
-        std::conditional_t<
-            Size == IdSize::U16Bit,
-            uint16_t,
-            uint32_t
-        >
-    >;
-
-    IdType() = default;
-    IdType(UintT id) : m_id(id) {}
-
-    [[nodiscard]] bool isEmpty() const {
-        return m_id == 0;
-    }
-
-    operator UintT() const {
-        return m_id;
-    }
-    bool operator==(const IdType& other) const {
-        return m_id == other.m_id;
-    }
-
-private:
-    UintT m_id{0};
-};
-
-namespace std {
-    template<IdSize Size>
-    struct hash<IdType<Size>> {
-        std::size_t operator()(const IdType<Size>& id) const noexcept {
-            return std::hash<typename IdType<Size>::UintT>()(static_cast<typename IdType<Size>::UintT>(id));
-        }
-    };
-}
 
 template<typename DataT, IdSize Size>
 class DataIdMapper {
---
+++ client/src/common/datastructures/IdType.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+enum class IdSize : uint8_t {
+    U8Bit = 8,
+    U16Bit = 16,
+    U20Bit = 20
+};
+
+template<IdSize Size>
+class IdType {
+public:
+    using UintT = std::conditional_t<Size == IdSize::U8Bit, uint8_t, std::conditional_t<Size == IdSize::U16Bit, uint16_t, uint32_t>>;
+
+    IdType() = default;
+    constexpr IdType(UintT id) : m_id(id) {}
+
+    [[nodiscard]] bool isEmpty() const {
+        return m_id == 0;
+    }
+
+    operator UintT() const {
+        return m_id;
+    }
+
+    bool operator==(const IdType& other) const {
+        return m_id == other.m_id;
+    }
+
+private:
+    UintT m_id{0};
+};
+
+namespace std {
+    template<IdSize Size>
+    struct hash<IdType<Size>> {
+        std::size_t operator()(const IdType<Size>& id) const noexcept {
+            return std::hash<typename IdType<Size>::UintT>()(static_cast<typename IdType<Size>::UintT>(id));
+        }
+    };
+}
