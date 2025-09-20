898030e015f7f8c6308f7ec8ba6870e9e4b32188
naszly <naszaly.k@gmail.com>
2025-09-20 17:51:24 +0200

    Flatten out array in KTreeNode to simplify node access

--- client/src/common/datastructures/KTreeNode.h
+++ client/src/common/datastructures/KTreeNode.h
@@ -71,7 +71,7 @@ public:
         if constexpr (IS_LEAF) {
             assert(x < NodeCountPerAxis && y < NodeCountPerAxis && z < NodeCountPerAxis);
             [[assume(x < NodeCountPerAxis && y < NodeCountPerAxis && z < NodeCountPerAxis)]];
-            return m_nodes[x][y][z];
+            return m_nodes[indexOf(x, y, z)];
         } else {
             const uint32_t nodeX = x / TREE_SIZE;
             const uint32_t nodeY = y / TREE_SIZE;
@@ -80,7 +80,7 @@ public:
             assert(nodeX < NodeCountPerAxis && nodeY < NodeCountPerAxis && nodeZ < NodeCountPerAxis);
             [[assume(nodeX < NodeCountPerAxis && nodeY < NodeCountPerAxis && nodeZ < NodeCountPerAxis)]];
 
-            const auto* child = m_nodes[nodeX][nodeY][nodeZ];
+            const auto* child = m_nodes[indexOf(nodeX, nodeY, nodeZ)];
             return child ? child->get(x % TREE_SIZE, y % TREE_SIZE, z % TREE_SIZE) : EMPTY;
         }
     }
@@ -89,7 +89,7 @@ public:
         if constexpr (IS_LEAF) {
             assert(x < NodeCountPerAxis && y < NodeCountPerAxis && z < NodeCountPerAxis);
             [[assume(x < NodeCountPerAxis && y < NodeCountPerAxis && z < NodeCountPerAxis)]];
-            m_nodes[x][y][z] = tData;
+            m_nodes[indexOf(x, y, z)] = tData;
         } else {
             const uint32_t nodeX = x / TREE_SIZE;
             const uint32_t nodeY = y / TREE_SIZE;
@@ -98,7 +98,7 @@ public:
             assert(nodeX < NodeCountPerAxis && nodeY < NodeCountPerAxis && nodeZ < NodeCountPerAxis);
             [[assume(nodeX < NodeCountPerAxis && nodeY < NodeCountPerAxis && nodeZ < NodeCountPerAxis)]];
 
-            auto*& child = m_nodes[nodeX][nodeY][nodeZ];
+            auto*& child = m_nodes[indexOf(nodeX, nodeY, nodeZ)];
             if (!child && !tData.isEmpty()) {
                 child = allocateChildNode();
             }
@@ -112,11 +112,11 @@ public:
     void forEach(Func&& func) {
         if constexpr (IS_LEAF) {
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                func((&m_nodes[0][0][0])[idx]);
+                func(m_nodes[idx]);
             }
         } else {
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                if (auto* child = (&m_nodes[0][0][0])[idx]) {
+                if (auto* child = m_nodes[idx]) {
                     child->forEach(func);
                 }
             }
@@ -127,11 +127,11 @@ public:
     void forEach(Func&& func) const {
         if constexpr (IS_LEAF) {
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                func((&m_nodes[0][0][0])[idx]);
+                func(m_nodes[idx]);
             }
         } else {
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                if (auto* child = (&m_nodes[0][0][0])[idx]) {
+                if (auto* child = m_nodes[idx]) {
                     child->forEach(func);
                 }
             }
@@ -140,19 +140,19 @@ public:
 
     void serialize(std::ostream& os) const {
         if constexpr (IS_LEAF) {
-            os.write(reinterpret_cast<const char*>(&m_nodes[0][0][0]), NODE_COUNT * sizeof(TData));
+            os.write(reinterpret_cast<const char*>(m_nodes), NODE_COUNT * sizeof(TData));
         } else {
             NodeBitmap hasChildrenBitmap;
 
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                if ((&m_nodes[0][0][0])[idx] != nullptr)
+                if (m_nodes[idx] != nullptr)
                     hasChildrenBitmap.set(idx);
             }
 
             os.write(hasChildrenBitmap.data(), hasChildrenBitmap.size());
 
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                if (const auto* child = (&m_nodes[0][0][0])[idx])
+                if (const auto* child = m_nodes[idx])
                     child->serialize(os);
             }
         }
@@ -160,13 +160,13 @@ public:
 
     void deserialize(std::istream& is) {
         if constexpr (IS_LEAF) {
-            is.read(reinterpret_cast<char*>(&m_nodes[0][0][0]), NODE_COUNT * sizeof(TData));
+            is.read(reinterpret_cast<char*>(m_nodes), NODE_COUNT * sizeof(TData));
         } else {
             NodeBitmap hasChildrenBitmap;
             is.read(hasChildrenBitmap.data(), hasChildrenBitmap.size());
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
                 if (hasChildrenBitmap.test(idx)) {
-                    auto*& child = (&m_nodes[0][0][0])[idx];
+                    auto*& child = m_nodes[idx];
                     if (!child) {
                         child = allocateChildNode();
                     }
@@ -179,10 +179,10 @@ public:
     void removeEmptyNodes() {
         if constexpr (!IS_LEAF) {
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                if (auto* child = (&m_nodes[0][0][0])[idx]) {
+                if (auto* child = m_nodes[idx]) {
                     if (child->isEmpty()) {
                         deallocateChildNode(child);
-                        (&m_nodes[0][0][0])[idx] = nullptr;
+                        m_nodes[idx] = nullptr;
                     } else {
                         child->removeEmptyNodes();
                     }
@@ -193,23 +193,26 @@ public:
 
     [[nodiscard]] bool isEmpty() const {
         if constexpr (IS_LEAF) {
-            return std::all_of(&m_nodes[0][0][0], &m_nodes[0][0][0] + NODE_COUNT, [](const TData& node) {
+            return std::all_of(m_nodes, m_nodes + NODE_COUNT, [](const TData& node) {
                 return node.isEmpty();
             });
         } else {
-            return std::all_of(&m_nodes[0][0][0], &m_nodes[0][0][0] + NODE_COUNT, [](const KTreeChildNode* child) {
+            return std::all_of(m_nodes, m_nodes + NODE_COUNT, [](const KTreeChildNode* child) {
                 return child == nullptr || child->isEmpty();
             });
         }
     }
 
     [[nodiscard]] const NodeType* getNodes() const {
-        return &m_nodes[0][0][0];
+        return m_nodes;
     }
 
 private:
+    NodeType m_nodes[NODE_COUNT]{};
 
-    NodeType m_nodes[NodeCountPerAxis][NodeCountPerAxis][NodeCountPerAxis]{};
+    static constexpr uint32_t indexOf(const uint32_t x, const uint32_t y, const uint32_t z) noexcept {
+        return (x * NodeCountPerAxis + y) * NodeCountPerAxis + z;
+    }
 
     static KTreeChildNode* allocateChildNode() {
 #if defined(USE_GLOBAL_BLOCK_ALLOCATOR)
@@ -239,18 +242,18 @@ private:
 
     void initialize() {
         if constexpr (IS_LEAF) {
-            std::fill_n(&m_nodes[0][0][0], NODE_COUNT, EMPTY);
+            std::fill_n(m_nodes, NODE_COUNT, EMPTY);
         } else {
-            std::fill_n(&m_nodes[0][0][0], NODE_COUNT, nullptr);
+            std::fill_n(m_nodes, NODE_COUNT, nullptr);
         }
     }
 
     void clear() {
         if constexpr (!IS_LEAF) {
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                if ((&m_nodes[0][0][0])[idx]) {
-                    deallocateChildNode((&m_nodes[0][0][0])[idx]);
-                    (&m_nodes[0][0][0])[idx] = nullptr;
+                if (m_nodes[idx]) {
+                    deallocateChildNode(m_nodes[idx]);
+                    m_nodes[idx] = nullptr;
                 }
             }
         }
@@ -263,15 +266,15 @@ private:
                 std::copy_n(&source.getNodes()[0], NODE_COUNT, &m_nodes[0][0][0]);
             } else {
                 for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
-                    (&m_nodes[0][0][0])[idx] = static_cast<TData>(source.getNodes()[idx]);
+                    m_nodes[idx] = static_cast<TData>(source.getNodes()[idx]);
                 }
             }
         } else {
             for (uint32_t idx = 0; idx < NODE_COUNT; ++idx) {
                 if (const auto* otherChild = source.getNodes()[idx]) {
-                    (&m_nodes[0][0][0])[idx] = allocateChildNode(*otherChild);
+                    m_nodes[idx] = allocateChildNode(*otherChild);
                 } else {
-                    (&m_nodes[0][0][0])[idx] = nullptr;
+                    m_nodes[idx] = nullptr;
                 }
             }
         }
@@ -280,9 +283,9 @@ private:
     void moveFrom(KTreeNode&& other) {
         std::copy_n(&other.m_nodes[0][0][0], NODE_COUNT, &m_nodes[0][0][0]);
         if constexpr (IS_LEAF) {
-            std::fill_n(&other.m_nodes[0][0][0], NODE_COUNT, EMPTY);
+            std::fill_n(other.m_nodes, NODE_COUNT, EMPTY);
         } else {
-            std::fill_n(&other.m_nodes[0][0][0], NODE_COUNT, nullptr);
+            std::fill_n(other.m_nodes, NODE_COUNT, nullptr);
         }
     }
 };
