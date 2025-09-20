5fd4376a1e9c5684fe8a561ad976521686996ae8
naszly <naszaly.k@gmail.com>
2025-09-20 10:25:13 +0200

    Allocate thread-local free list on heap via unique_ptr & increase buffer size

--- client/src/common/datastructures/ThreadLocalBlockAllocator.h
+++ client/src/common/datastructures/ThreadLocalBlockAllocator.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cstddef>
-#include "common/Thread.h"
+#include <cstdlib>
+#include <memory>
+
 #include "common/datastructures/AtomicQueue.h"
 #include "common/datastructures/CircularBuffer.h"
 
@@ -16,7 +18,7 @@ public:
     }
 
     void* allocate() {
-        if (const auto ptr = m_threadLocalFreeList.tryPop()) {
+        if (const auto ptr = m_threadLocalFreeList->tryPop()) {
             return ptr.value();
         }
 
@@ -28,20 +30,22 @@ public:
     }
 
     void deallocate(void* ptr) {
-        if (m_threadLocalFreeList.full()) {
+        if (m_threadLocalFreeList->full()) {
             spillToGlobalFreeList();
         }
 
-        m_threadLocalFreeList.push(ptr);
+        m_threadLocalFreeList->push(ptr);
     }
 
 private:
-    static constexpr size_t MAX_THREAD_LOCAL_BLOCKS = BlocksPerChunk * 32;
+    static constexpr size_t MAX_THREAD_LOCAL_BLOCKS = BlocksPerChunk * 256;
 
     AtomicQueue<std::byte*> m_chunks;
     AtomicQueue<void*> m_globalFreeList;
 
-    static thread_local CircularBuffer<void*, MAX_THREAD_LOCAL_BLOCKS> m_threadLocalFreeList;
+    using ThreadLocalFreeList = CircularBuffer<void*, MAX_THREAD_LOCAL_BLOCKS>;
+    using ThreadLocalFreeListPtr = std::unique_ptr<ThreadLocalFreeList>;
+    static thread_local ThreadLocalFreeListPtr m_threadLocalFreeList;
 
     void* allocateNewChunk() {
         constexpr std::size_t alignment = alignof(std::max_align_t);
@@ -57,19 +61,20 @@ private:
     static void populateThreadLocalFreeList(std::byte* newChunk) {
         const std::byte* end = newChunk + BlockSize * BlocksPerChunk;
         for (std::byte* ptr = newChunk + BlockSize; ptr < end; ptr += BlockSize) {
-            m_threadLocalFreeList.push(ptr);
+            m_threadLocalFreeList->push(ptr);
         }
     }
 
     void spillToGlobalFreeList() {
         void* freeList[BlocksPerChunk];
         for (size_t i = 0; i < BlocksPerChunk; ++i) {
-            freeList[i] = m_threadLocalFreeList.pop();
+            freeList[i] = m_threadLocalFreeList->pop();
         }
         m_globalFreeList.pushMultiple(freeList);
     }
 };
 
 template<size_t BlockSize, size_t BlocksPerChunk>
-thread_local CircularBuffer<void*, ThreadLocalBlockAllocator<BlockSize, BlocksPerChunk>::MAX_THREAD_LOCAL_BLOCKS>
-    ThreadLocalBlockAllocator<BlockSize, BlocksPerChunk>::m_threadLocalFreeList = {};
+thread_local ThreadLocalBlockAllocator<BlockSize, BlocksPerChunk>::ThreadLocalFreeListPtr
+    ThreadLocalBlockAllocator<BlockSize, BlocksPerChunk>::m_threadLocalFreeList =
+        std::make_unique<ThreadLocalFreeList>();
