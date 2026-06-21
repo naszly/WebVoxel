660ba97c233f2a2aeb128d17cdf7ab6f9840e821
naszly <naszaly.k@gmail.com>
2026-06-21 11:44:48 +0200

    Fix logging format in Exception class to use a compile-time format string

--- client/src/common/Exception.h
+++ client/src/common/Exception.h
@@ -10,13 +10,13 @@
 class Exception final : public std::exception {
 public:
     explicit Exception(std::string message) noexcept : m_message(std::move(message)) {
-        LogCore::critical(m_message);
+        LogCore::critical("{}", m_message);
     }
 
     template <typename... Args>
     explicit Exception(std::format_string<Args...> format, Args&&... args) noexcept
         : m_message(std::format(format, std::forward<Args>(args)...)) {
-        LogCore::critical(m_message);
+        LogCore::critical("{}", m_message);
     }
 
     [[nodiscard]] const char* what() const noexcept override {
